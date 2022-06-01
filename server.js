f4c96456a6e7d94e5ff3b559dfe65a6ded263b39
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-06-01 16:01:02 +0100

    Added mongoose database connection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 // Initialise express
 const app = express();
 // Listen for environment variable for port, or use port 5000
 const PORT = process.env.PORT || 5000;
+// Connect to database
+connectDB();
 // Get request for API
 app.get('/',(req,res) => res.send('API Running'));
 
